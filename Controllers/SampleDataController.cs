d73540602af634e84e24921c6f8d37c255cddad4
Vahid <Vahid.jafari@outlook.com>
2019-06-11 12:32:26 +0430

    solution creation and the test.html adding

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -9,7 +9,7 @@ namespace TestMakerFree.Controllers
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
-        private static string[] Summaries = new[]
+        private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
@@ -31,14 +31,7 @@ namespace TestMakerFree.Controllers
             public string DateFormatted { get; set; }
             public int TemperatureC { get; set; }
             public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
         }
     }
 }
